f19f8f2f61c199e27931a6f38aa85f74131c0777
Sahil Salim <sal@Sahils-MacBook-Air.local>
2023-05-05 01:01:03 +0530

    disabled difficulty

--- content.js
+++ content.js
@@ -1,21 +1,32 @@
-const observer = new MutationObserver(function(mutationsList) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-        console.log("RANNN")
-        const likes = document.querySelector("#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(2) > span")
-        const dislikes = document.querySelector("#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(3) > span")
-        if (likes) {
-          likes.style.display = 'none';
-        }
-        if (dislikes) {
-          dislikes.style.display = 'none';
-        }
-        if (likes && dislikes) {
-          observer.disconnect();
-        }
+const observer = new MutationObserver(function (mutationsList) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+      document.querySelector("#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(2) > svg").style.display = "none"
+      document.querySelector("#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(3) > svg").style.display = "none"
+
+      const likes = document.querySelector(
+        "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(2) > span"
+      );
+      const dislikes = document.querySelector(
+        "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > button:nth-child(3) > span"
+      );
+      const difficulty = document.querySelector(
+        "#app > div > div.main__2_tD > div.content__3fR6 > div > div.side-tools-wrapper__1TS9 > div > div.css-1gd46d6-Container.e5i1odf0 > div.css-jtoecv > div > div.tab-pane__ncJk.css-1eusa4c-TabContent.e5i1odf5 > div > div.css-101rr4k > div.css-10o4wqw > div"
+      );
+      if (likes) {
+        likes.style.display = "none";
+      }
+      if (dislikes) {
+        dislikes.style.display = "none";
+      }
+      if (difficulty) {
+        difficulty.style.display = "none"
+      }
+      if (likes && dislikes) {
+        observer.disconnect();
       }
     }
-  });
-  
-  observer.observe(document.body, { childList: true, subtree: true });
-  
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
